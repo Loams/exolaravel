ad29d7948e708ea079fe2c0cf0c71888c8a16703
Loams <stephane.mullings@gmail.com>
2016-09-24 17:48:07 +0200

    modification en utilisant le system de repositories

--- tuto-laravel/app/Http/Controllers/EmailController.php
+++ tuto-laravel/app/Http/Controllers/EmailController.php
@@ -6,20 +6,23 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-use App\Email;
 
 use App\Http\Requests\EmailRequest;
 
+use App\Repositories\EmailRepository;
+
 class EmailController extends Controller
 {
-    public function getForm(){
+    public function getForm()
+    {
     	return view('email');
     }
 	
-	public function postForm(EmailRequest $request){
-		$email = new Email;
-		$email->email=$request->input('email');
-		$email->save();
+	public function postForm(
+		EmailRequest $request, 
+		EmailRepository $emailRepository)
+	{
+		$emailRepository->save($request->input('email'));
 		return view('email_ok');
 	} 
 }
